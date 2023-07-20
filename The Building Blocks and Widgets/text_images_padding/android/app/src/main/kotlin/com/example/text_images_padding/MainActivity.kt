4824be0a313ef0165d8ed14e696d4b7329cd76e3
Arpita Gupta <arpitagupta6620@gmail.com>
2023-07-20 21:19:47 +0530

    Learning about widgets, text and paading

---
+++ The Building Blocks and Widgets/text_images_padding/android/app/src/main/kotlin/com/example/text_images_padding/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.text_images_padding
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ The Building Blocks and Widgets/text_images_padding/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          //text wrapped with center widget
+          child: Text(
+            "Hello", //simply passing Text as a widget
+            //adding style to text
+            style: TextStyle(
+              color: Colors.teal,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
