92e468a9575429471131c3a55213bf12a99dba35
Arpita Gupta <arpitagupta6620@gmail.com>
2023-07-21 17:55:33 +0530

    Created Rows and Columns

--- The Building Blocks and Widgets/text_images_padding/lib/main.dart
+++ The Building Blocks and Widgets/text_images_padding/lib/main.dart
@@ -12,18 +12,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String str = "Hey! My name is Arpita!";
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          //text wrapped with center widget
-          child: Text(
-            "Hello", //simply passing Text as a widget
-            //adding style to text
-            style: TextStyle(
-              color: Colors.teal,
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(
+                  "https://e0.pxfuel.com/wallpapers/91/471/desktop-wallpaper-18-nature-with-bird.jpg"),
+              //Image.asset("When image is stored in assets folder"),
+              // Image.memory("When image icon is stored in bytes")
+              Chip(
+                //adding chip
+                label: Text(
+                  str, //simply passing Text as a widget
+                  //adding style to text
+                  style: const TextStyle(
+                    color: Colors.teal,
+                    fontWeight: FontWeight.bold, //bold text
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: OutlinedButton(
+                  child: const Text("Press Me"),
+                  onPressed: () {
+                    str = "You just pressed the button";
+                    setState(() {});
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
