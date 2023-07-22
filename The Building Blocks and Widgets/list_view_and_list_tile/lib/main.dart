1a9e27d89aa323224985b21ff74112ce9a283223
Arpita Gupta <arpitagupta6620@gmail.com>
2023-07-22 14:01:38 +0530

    Completed List View

--- The Building Blocks and Widgets/list_view_and_list_tile/lib/main.dart
+++ The Building Blocks and Widgets/list_view_and_list_tile/lib/main.dart
@@ -92,13 +92,50 @@ class _MyAppState extends State<MyApp> {
           ],
         ), */
 
-        body: ListView.builder(
+        /*  body: ListView.builder(
           itemBuilder: (BuildContext context, int index) {
             return ListTile(
-                //3:54
-                );
+              title: Text("List Count no" + (index + 1).toString()),
+            );
           },
-        ),
+          itemCount: 20,
+        ), */
+
+        /* body: ListView.custom(
+            childrenDelegate: SliverChildListDelegate([
+          const ListTile(
+            title: Text("First Tile"),
+          )
+        ])), */
+
+        body: ListView.separated(
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text("List Tile No" + (index + 1).toString()),
+                /* onTap: () {
+                  print(index);
+                }, */
+                onLongPress: () {
+                  print("You long pressed on the tile");
+                },
+                dense: true, //to consume less space,
+                leading:
+                    const Icon(Icons.add), // add icon to every line before text
+                trailing: const Icon(
+                    Icons.pin_drop), //add icon at end of every line after text
+              );
+            },
+            separatorBuilder: (context, index) {
+              if ((index + 1) % 5 == 0) {
+                return ListTile(
+                  title: Text((index + 1).toString()),
+                  selected: true,
+                );
+              } else {
+                return Container();
+              }
+            },
+            itemCount: 30),
       ),
     );
   }
