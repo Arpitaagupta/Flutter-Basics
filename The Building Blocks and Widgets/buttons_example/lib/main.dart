e38b67778453830bb8ab3d92c3a2fd9698e1fb51
Arpita Gupta <arpitagupta6620@gmail.com>
2023-07-23 21:46:54 +0530

    Modified button

--- The Building Blocks and Widgets/buttons_example/lib/main.dart
+++ The Building Blocks and Widgets/buttons_example/lib/main.dart
@@ -4,31 +4,36 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
           child: TextButton(
-            style: TextButton.styleFrom(
-              primary: Colors.white,
-              backgroundColor: Colors.teal,
-              //unable to add highlightColor : Colors.yellow
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.teal),
+              overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                (Set<MaterialState> states) {
+                  if (states.contains(MaterialState.hovered))
+                    return Colors.grey.withOpacity(0.04);
+                  if (states.contains(MaterialState.focused) ||
+                      states.contains(MaterialState.pressed))
+                    return Colors.yellow.withOpacity(1.0);
+                  return null; // Defer to the widget's default.
+                },
+              ),
             ),
+            onPressed: () {
+              print("Button Pressed"); // Add your desired action here
+            },
             child: const Text("Press Me"),
-            onPressed: () {},
           ),
         ),
       ),
     );
   }
 }
-//
