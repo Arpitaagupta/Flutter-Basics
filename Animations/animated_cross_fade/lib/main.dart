56a6b8490c90e8156be3d497aadc278b11cd6f67
Arpita Gupta <arpitagupta6620@gmail.com>
2023-07-27 08:39:47 +0530

    Animated Cross Fade completed

--- Animations/animated_cross_fade/lib/main.dart
+++ Animations/animated_cross_fade/lib/main.dart
@@ -33,29 +33,58 @@ class Main extends StatefulWidget {
   _MainState createState() => _MainState();
 }
 
-class _MainState extends State<Main> with SingleTickerProviderStateMixin {
+class _MainState extends State<Main> {
+  bool first = true;
   @override
   void initState() {
     super.initState();
+    first = false; //
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: AnimatedCrossFade(
-          firstChild: FlutterLogo(
-            style: FlutterLogoStyle.stacked,
-            size: 100.0,
-          ),
-          secondChild: FlutterLogo(
-            style: FlutterLogoStyle.horizontal,
-            size: 200.0,
-          ),
-          crossFadeState: CrossFadeState.showFirst,
-          duration: Duration(
-            seconds: 2,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Container(
+                // Set the background color here
+                color: Colors.blue,
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      // Toggle the 'first' value when the button is pressed.
+                      first = !first;
+                    });
+                  },
+                  child: const Text(
+                    "Animate",
+                    style: TextStyle(
+                      color: Colors.white, //  Set text color
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            AnimatedCrossFade(
+              firstChild: const FlutterLogo(
+                style: FlutterLogoStyle.stacked,
+                size: 100.0,
+              ),
+              secondChild: const FlutterLogo(
+                style: FlutterLogoStyle.horizontal,
+                size: 200.0,
+              ),
+              crossFadeState:
+                  first ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+              duration: const Duration(
+                seconds: 2, //to sped up the transition --> use miliseconds
+              ),
+            ),
+          ],
         ),
       ),
     );
