ff028f91e6af051bb01647bf5d5a78286ca35072
Arpita Gupta <arpitagupta6620@gmail.com>
2023-07-28 16:08:36 +0530

    Started learning HTTP Package

---
+++ HTTP Pacakge/add_http_package/android/app/src/main/kotlin/com/example/add_http_package/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.add_http_package
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ HTTP Pacakge/add_http_package/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    // http.get(url)
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
