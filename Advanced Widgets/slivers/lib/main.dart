142e70dc763dda29c00ac93861d750910ce44f10
Arpita Gupta <arpitagupta6620@gmail.com>
2023-07-30 21:49:36 +0530

    Created slivers

---
+++ Advanced Widgets/slivers/lib/main.dart
@@ -0,0 +1,49 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainPage());
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class Main extends StatefulWidget {
+  const Main({super.key});
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        // Slivers is the child of CustomScrollView
+        slivers: [
+          SliverAppBar(
+              //appbar with some extra features
+              )
+        ],
+      ),
+    );
+  }
+}
