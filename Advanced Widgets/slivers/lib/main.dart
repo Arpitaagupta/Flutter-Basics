54d775ad088bef4f39e4b767babf9321b93e222c
Arpita Gupta <arpitagupta6620@gmail.com>
2023-07-30 23:11:03 +0530

    Created sliverAppbar and added various properties to it

--- Advanced Widgets/slivers/lib/main.dart
+++ Advanced Widgets/slivers/lib/main.dart
@@ -16,7 +16,7 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Main();
   }
 }
 
@@ -35,14 +35,49 @@ class _MainState extends State<Main> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        // Slivers is the child of CustomScrollView
-        slivers: [
-          SliverAppBar(
+    return MaterialApp(
+      home: Scaffold(
+        body: CustomScrollView(
+          // Slivers is the child of CustomScrollView
+          slivers: [
+            SliverAppBar(
               //appbar with some extra features
-              )
-        ],
+              //speciality of sliverappbar : when we scroll down appbar also gets scrolled
+              title: Text("Hey I am the app"),
+
+              pinned:
+                  true, //now it will act like normal appbar, it won't scroll,
+              floating:
+                  true, // on scroll down --> app bar will also be scrolled down along with list,
+              centerTitle: true, //title will appear  at center,
+              elevation: 10.0, //shadow,
+              forceElevated: true,
+              // forceElevated: shadow will always exist even if there is nothing below the appbar,
+              expandedHeight: 200.0,
+              //expanded height --> increases height of appbar on scrolling down and comes back to original size on scrolling up
+              flexibleSpace: FlexibleSpaceBar(
+                background: Image.network(
+                  "https://images.pexels.com/photos/5556562/pexels-photo-5556562.jpeg?auto=compress&cs=tinysrgb&w=600",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            //just like normal list we have sliver list,
+
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  return ListTile(
+                    title: Text("I am list tile no " + index.toString()),
+                    leading: const Icon(Icons.face),
+                  );
+                },
+                childCount: 30,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
