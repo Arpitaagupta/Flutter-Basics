acccb77ebafbed5af8e2bc396c4f0222799c2c56
Arpita Gupta <arpitagupta6620@gmail.com>
2023-08-04 17:16:44 +0530

    Assigment 5 completed

---
+++ Advanced Widgets/assignment_5/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+
+void main() {
+  runApp(const MainPage());
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const Main(),
+      theme: ThemeData(
+        primaryColor: Colors.amber,
+        appBarTheme: const AppBarTheme(
+          color: Colors.amber,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+} //
+
+class Main extends StatefulWidget {
+  const Main({super.key});
+
+  @override
+  _MainState createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  void initState() {
+    super.initState();
+    // Add your initialization code here
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("I am the AppBar"),
+        centerTitle: true,
+        toolbarHeight: 60.0,
+      ),
+      body: Stack(
+        children: [
+          ClipPath(
+            clipper: WaveClipperOne(),
+            child: Container(
+              color: Theme.of(context).primaryColor,
+              height: 60.0,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
