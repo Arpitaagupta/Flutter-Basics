9eac6ea1aebdcdb1aa50713188110ac344311c3a
Arpita Gupta <arpitagupta6620@gmail.com>
2023-08-04 14:34:08 +0530

    Redux Assignment completed

---
+++ Redux/assignment_6/lib/counterState.dart
@@ -0,0 +1,5 @@
+class CounterState {
+  final int counter;
+  CounterState(this.counter); //initialized counter using constructor
+}
+//we have to make model for our redux package everytime
---
+++ Redux/assignment_6/lib/reducer.dart
@@ -0,0 +1,17 @@
+import 'counterState.dart';
+import 'main.dart';
+
+// The redux package uses reducers to perform actions on data
+CounterState reducers(CounterState counterState, action) {
+  if (action == Action.Inc) {
+    return CounterState(counterState.counter + 1);
+  } else if (action == Action.Dec) {
+    return CounterState(counterState.counter - 1);
+  } else if (action == Action.Double) {
+    return CounterState(counterState.counter * 2);
+  } else if (action == Action.Half) {
+    return CounterState(
+        counterState.counter ~/ 2); // to convert into integer --> ~/
+  }
+  return counterState;
+}
