4bc3530cdac805c7c4f4507c44b349c40f6f2b78
Arpita Gupta <arpitagupta6620@gmail.com>
2023-08-04 11:28:55 +0530

    Modified Redux Example

--- Redux/redux_example1/lib/main.dart
+++ Redux/redux_example1/lib/main.dart
@@ -9,7 +9,12 @@ void main() {
 }
 
 //
-enum Action { Inc } //Inc stands for increment
+enum Action {
+  Inc, // Increment action
+  Dec, // Decrement action
+  Double, //Value will be doubled
+  Half, //Value will be halved
+}
 
 //we will not use stateful widget, we will use redux package
 class MainApp extends StatelessWidget {
@@ -17,26 +22,86 @@ class MainApp extends StatelessWidget {
 
   //const
   MainApp({super.key});
-  final Store store =
+  final Store<CounterState> store =
       Store<CounterState>(reducers, initialState: CounterState(0));
 
   @override
   Widget build(BuildContext context) {
-    return StoreProvider(
+    return StoreProvider<CounterState>(
       store: store,
       child: MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Redux Example"),
-          ),
-          body: Column(
-            children: [
-              const Text("The value of counter is " + "0"),
-              TextButton(onPressed: () {}, child: const Text("Add"))
-            ],
-          ),
-        ),
+            appBar: AppBar(
+              title: const Text("Redux Example"),
+            ),
+            body: ColumnData()),
       ),
+    );
+  }
+}
+
+/*
+We need to wrap whole in a widget StoreProvider, but where we want to use that values we need to wrap
+that widget in StoreConnector
+ */
+
+class ColumnData extends StatelessWidget {
+  const ColumnData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<CounterState, CounterState>(
+      converter: (store) => store.state,
+      builder: (context, state) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("The value of counter is " + state.counter.toString()),
+            StoreConnector<CounterState, Function>(
+              converter: (store) {
+                return (action) {
+                  // Dispatch the Inc action when the callback is called
+                  store.dispatch(action);
+                };
+              },
+              builder: (context, callback) {
+                return Column(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        callback(Action
+                            .Inc); //it will dispatch a function with action increment
+                      },
+                      child: const Text("Add"),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        callback(Action
+                            .Dec); //it will dispatch a function with action increment
+                      },
+                      child: const Text("Dec"),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        callback(Action
+                            .Double); //it will dispatch a function with action increment
+                      },
+                      child: const Text("Double"),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        callback(Action
+                            .Half); //it will dispatch a function with action increment
+                      },
+                      child: const Text("Half"),
+                    ),
+                  ],
+                );
+              },
+            )
+          ],
+        );
+      },
     );
   }
 }
--- Redux/redux_example1/lib/reducer.dart
+++ Redux/redux_example1/lib/reducer.dart
@@ -5,6 +5,13 @@ import 'package:redux_example1/main.dart';
 CounterState reducers(CounterState counterState, action) {
   if (action == Action.Inc) {
     return CounterState(counterState.counter + 1);
+  } else if (action == Action.Dec) {
+    return CounterState(counterState.counter - 1);
+  } else if (action == Action.Double) {
+    return CounterState(counterState.counter * 2);
+  } else if (action == Action.Half) {
+    return CounterState(
+        counterState.counter ~/ 2); // to convert into integer --> ~/
   }
   return counterState;
 }
