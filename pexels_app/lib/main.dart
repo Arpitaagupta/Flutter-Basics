9e16db54aa80827639818f379197853badbfe969
Arpita Gupta <arpitagupta6620@gmail.com>
2023-08-06 12:15:34 +0530

    Added http package

--- pexels_app/lib/main.dart
+++ pexels_app/lib/main.dart
@@ -22,7 +22,17 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController controller;
+  String ul = "https://www.pexels.com/api/documentation/#photos-search";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
